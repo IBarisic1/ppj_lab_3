1e32b1976034ca55486754225ae3480af9e2503b
Luka Banović <luka.banovic@fer.hr>
2017-12-24 09:19:55 +0100

    dodana listaArgumenata

--- lab3/src/main/java/hr/fer/ppj/labos/lab3/ListaArgumenata.java
+++ lab3/src/main/java/hr/fer/ppj/labos/lab3/ListaArgumenata.java
@@ -15,7 +15,22 @@ public class ListaArgumenata implements CvorAtributnogStabla{
 	}
 	
 	public void provjeri() {
-		
+		String desnaStranaProdukcije = trenutniCvor.desnaStranaProdukcije();
+		if(desnaStranaProdukcije.equals("<izraz_pridruzivanja>")) {
+			IzrazPridruzivanja izrazPridruzivanja = new IzrazPridruzivanja(
+					trenutniCvor.getDjeca().get(0));
+			izrazPridruzivanja.provjeri();
+			tipovi.clear();
+			tipovi.add(izrazPridruzivanja.getTip());
+		}else if(desnaStranaProdukcije.equals("<lista_argumenata> ZAREZ <izraz_pridruzivanja>")) {
+			ListaArgumenata listaArgumenata = new ListaArgumenata(trenutniCvor.getDjeca().get(0));
+			listaArgumenata.provjeri();
+			IzrazPridruzivanja izrazPridruzivanja = new IzrazPridruzivanja(
+					trenutniCvor.getDjeca().get(2));
+			izrazPridruzivanja.provjeri();
+			tipovi = new ArrayList<>(listaArgumenata.getTipovi());
+			tipovi.add(izrazPridruzivanja.getTip());
+		}
 	}
 
 	public List<String> getTipovi() {

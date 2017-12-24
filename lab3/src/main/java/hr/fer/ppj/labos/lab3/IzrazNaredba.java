6bc43273c8cdf65a8b5b16e07187cbdc1419d6e9
Ivan Barišić <ivan.barisic@fer.hr>
2017-12-24 17:45:37 +0100

    Dodana NaredbaPetlje i dodan getTip u IzrazNaredba

--- lab3/src/main/java/hr/fer/ppj/labos/lab3/IzrazNaredba.java
+++ lab3/src/main/java/hr/fer/ppj/labos/lab3/IzrazNaredba.java
@@ -1,22 +1,26 @@
 package hr.fer.ppj.labos.lab3;
 
-public class IzrazNaredba implements CvorAtributnogStabla{
-	
+public class IzrazNaredba implements CvorAtributnogStabla {
+
 	private String tip;
-	
+
 	private CvorGenerativnogStabla trenutniCvor;
-	
+
 	public IzrazNaredba(CvorGenerativnogStabla trenutniCvor) {
 		this.trenutniCvor = trenutniCvor;
 	}
-	
+
+	public String getTip() {
+		return tip;
+	}
+
 	public void provjeri() {
 		if (trenutniCvor.desnaStranaProdukcije().equals("TOCKAZAREZ")) {
 			tip = "int";
 		} else if (trenutniCvor.desnaStranaProdukcije().equals("<izraz> TOCKAZAREZ")) {
 			Izraz izraz = new Izraz(trenutniCvor.getDjeca().get(0));
 			izraz.provjeri();
-			
+
 			tip = izraz.getTip();
 		}
 	}
--- lab3/src/main/java/hr/fer/ppj/labos/lab3/NaredbaPetlje.java
+++ lab3/src/main/java/hr/fer/ppj/labos/lab3/NaredbaPetlje.java
@@ -1,14 +1,55 @@
 package hr.fer.ppj.labos.lab3;
 
-public class NaredbaPetlje implements CvorAtributnogStabla{
-	
+public class NaredbaPetlje implements CvorAtributnogStabla {
+
 	private CvorGenerativnogStabla trenutniCvor;
-	
+
 	public NaredbaPetlje(CvorGenerativnogStabla trenutniCvor) {
 		this.trenutniCvor = trenutniCvor;
 	}
-	
+
 	public void provjeri() {
-		
+		if (trenutniCvor.desnaStranaProdukcije().equals("KR_WHILE L_ZAGRADA <izraz> D_ZAGRADA <naredba>")) {
+			Izraz izraz = new Izraz(trenutniCvor.getDjeca().get(2));
+			izraz.provjeri();
+
+			if (!SemantickiAnalizator.implicitnaPretvorba.get(izraz.getTip()).contains("int")) {
+				SemantickiAnalizator.ispisiGreskuUProdukciji(trenutniCvor);
+			}
+
+			Naredba naredba = new Naredba(trenutniCvor.getDjeca().get(4));
+			naredba.provjeri();
+		} else if (trenutniCvor.desnaStranaProdukcije()
+				.equals("KR_FOR L_ZAGRADA <izraz_naredba> <izraz_naredba> D_ZAGRADA <naredba>")) {
+			IzrazNaredba izrazNaredba1 = new IzrazNaredba(trenutniCvor.getDjeca().get(2));
+			izrazNaredba1.provjeri();
+
+			IzrazNaredba izrazNaredba2 = new IzrazNaredba(trenutniCvor.getDjeca().get(3));
+			izrazNaredba2.provjeri();
+
+			if (!SemantickiAnalizator.implicitnaPretvorba.get(izrazNaredba2.getTip()).contains("int")) {
+				SemantickiAnalizator.ispisiGreskuUProdukciji(trenutniCvor);
+			}
+
+			Naredba naredba = new Naredba(trenutniCvor.getDjeca().get(5));
+			naredba.provjeri();
+		} else if (trenutniCvor.desnaStranaProdukcije()
+				.equals("KR_FOR L_ZAGRADA <izraz_naredba> <izraz_naredba> <izraz> D_ZAGRADA <naredba>")) {
+			IzrazNaredba izrazNaredba1 = new IzrazNaredba(trenutniCvor.getDjeca().get(2));
+			izrazNaredba1.provjeri();
+
+			IzrazNaredba izrazNaredba2 = new IzrazNaredba(trenutniCvor.getDjeca().get(3));
+			izrazNaredba2.provjeri();
+
+			if (!SemantickiAnalizator.implicitnaPretvorba.get(izrazNaredba2.getTip()).contains("int")) {
+				SemantickiAnalizator.ispisiGreskuUProdukciji(trenutniCvor);
+			}
+			
+			Izraz izraz = new Izraz(trenutniCvor.getDjeca().get(4));
+			izraz.provjeri();
+			
+			Naredba naredba = new Naredba(trenutniCvor.getDjeca().get(6));
+			naredba.provjeri();
+		}
 	}
 }
